0af2cf9df0f47bcf69d00a50c5a5886790e8da1d
Steven Rockarts <stevenrockarts@gmail.com>
2022-03-26 17:49:06 -0600

    Receive iOS Notifications

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,31 +8,25 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
       GeneratedPluginRegistrant.register(with: self)
+      
+      if #available(iOS 10.0, *) {
+        // For iOS 10 display notification (sent via APNS)
+        UNUserNotificationCenter.current().delegate = self
+
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+          options: authOptions,
+          completionHandler: { _, _ in }
+        )
+      } else {
+        let settings: UIUserNotificationSettings =
+          UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+        application.registerUserNotificationSettings(settings)
+      }
+
+      application.registerForRemoteNotifications()
+
+      
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-    
-//    override func registerForRemoteNotifications() {
-//        if ([UNUserNotificationCenter class] != nil) {
-//          // iOS 10 or later
-//          // For iOS 10 display notification (sent via APNS)
-//          [UNUserNotificationCenter currentNotificationCenter].delegate = self;
-//          UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert |
-//              UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
-//          [[UNUserNotificationCenter currentNotificationCenter]
-//              requestAuthorizationWithOptions:authOptions
-//              completionHandler:^(BOOL granted, NSError * _Nullable error) {
-//                // ...
-//              }];
-//        } else {
-//          // iOS 10 notifications aren't available; fall back to iOS 8-9 notifications.
-//          UIUserNotificationType allNotificationTypes =
-//          (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge);
-//          UIUserNotificationSettings *settings =
-//          [UIUserNotificationSettings settingsForTypes:allNotificationTypes categories:nil];
-//          [application registerUserNotificationSettings:settings];
-//        }
-//
-//        [application registerForRemoteNotifications];
-//
-//    }
 }
